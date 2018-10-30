2a604e3224e8b5548252cdc05ff7a3471f0a05cb
mkrzan <maciej.krzanowski@flipsports.com>
2018-10-30 11:29:17 +0100

    Wallet Page

---
+++ src/WalletPage/WalletPage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { userActions } from "../_actions";
+
+class WalletPage extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(userActions.getAll());
+  }
+
+  handleDeleteUser(id) {
+    return e => this.props.dispatch(userActions.delete(id));
+  }
+
+  render() {
+    const { user, users } = this.props;
+    return (
+      <div className="col-md-6 col-md-offset-3">
+        <h1>Hi Ziom</h1>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const { users, authentication } = state;
+  const { user } = authentication;
+  return {
+    user,
+    users
+  };
+}
+
+const connectedHomePage = connect(mapStateToProps)(WalletPage);
+export { connectedHomePage as WalletPage };
