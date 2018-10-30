d8e9e4046d37e63afedc034b4f8caa58b24e059a
mkrzan <maciej.krzanowski@flipsports.com>
2018-10-30 11:29:09 +0100

    Added NavBar to App

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -8,6 +8,7 @@ import { PrivateRoute } from "../_components";
 import { HomePage } from "../HomePage";
 import { LoginPage } from "../LoginPage";
 import { RegisterPage } from "../RegisterPage";
+import { WalletPage } from "../WalletPage";
 import { NavBar } from "../NavBar";
 
 function withBaseFix(HashRouter) {
@@ -47,22 +48,24 @@ class App extends React.Component {
     const { alert } = this.props;
     const FixedHashRouter = withBaseFix(HashRouter);
     return (
-      <div className="jumbotron">
-        <div className="container">
-          <div className="col-sm-8 col-sm-offset-2">
-            {alert.message && (
-              <div className={`alert ${alert.type}`}>{alert.message}</div>
-            )}
-            <FixedHashRouter history={history}>
-              <div>
+      <FixedHashRouter history={history}>
+        <div>
+          <NavBar />
+          <div className="jumbotron">
+            <div className="container">
+              <div className="col-sm-8 col-sm-offset-2">
+                {alert.message && (
+                  <div className={`alert ${alert.type}`}>{alert.message}</div>
+                )}
                 <PrivateRoute exact path="/" component={HomePage} />
                 <Route path="/login" component={LoginPage} />
                 <Route path="/register" component={RegisterPage} />
+                <Route path="/wallet_page" component={WalletPage} />
               </div>
-            </FixedHashRouter>
+            </div>
           </div>
         </div>
-      </div>
+      </FixedHashRouter>
     );
   }
 }
--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,33 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-// Stateless Functional Component
-const NavBar = () => {
-  return (
-    <nav className="navbar navbar-inverse">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <p className="navbar-brand">CS-ADMIN</p>
+class NavBar extends Component {
+  render() {
+    const user = this.props.authentication.user;
+
+    return (
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <p className="navbar-brand">CS-ADMIN</p>
+          </div>
+          <ul className="nav navbar-nav navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/wallet_page">Wallet Lines</Link>
+            </li>
+            <li>
+              <Link to="/schedule">Schedule</Link>
+            </li>
+          </ul>
+          <ul className="nav navbar-nav navbar-right navigation">
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
         </div>
-        <ul className="nav navbar-nav navigation">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/wallet_lines">Wallet Lines</Link>
-          </li>
-          <li>
-            <Link to="/schedule">Schedule</Link>
-          </li>
-        </ul>
-        <ul className="nav navbar-nav navbar-right navigation">
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
+      </nav>
+    );
+  }
+}
 
-export default NavBar;
+function mapStateToProps(state) {
+  const { authentication } = state;
+  return {
+    authentication
+  };
+}
+
+const connectedNavigationBar = connect(mapStateToProps)(NavBar);
+export { connectedNavigationBar as NavBar };
